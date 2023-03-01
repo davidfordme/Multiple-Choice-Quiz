a335a5deac532c4023d0a1e5c1a763c1a004c80f
David Ford <d.ford@digital-velocity.co.uk>
2023-03-01 00:53:07 +0000

    Initial quiz setup

--- src/routes/quiz/+page.svelte
+++ src/routes/quiz/+page.svelte
@@ -1,5 +1,57 @@
 <script>
+	import quiz from '$lib/data/quiz-1.json';
+
+	const questions = quiz.questions;
+	var questionData = [];
+
+	function* shuffle(arr) {
+		arr = [...arr];
+		while(arr.length) yield arr.splice(Math.random()*arr.length|0, 1)[0]
+	}
+
+	function setupQuestion(answersArray) {
+
+		var shuffledArray = [...shuffle(answersArray)];
+
+		shuffledArray.map((answer, index) => {
+			questionData.push({
+				index : index,
+				answer : answer,
+				selected : false,
+				correct : null
+			})
+		})
+	}
 	
+	function handleAnswerClick (event) {
+		let selectedAnswer = questionData[event.target.getAttribute('data-index')]
+		selectedAnswer.selected = true;
+		
+		if(event.target.innerText.toLowerCase() == selectedQuestion.answer) {
+			revealAllAnswers()
+			selectedAnswer.correct = true;
+		} else {
+			selectedAnswer.correct = false;
+		}
+
+		questionData[event.target.getAttribute('data-index')] = selectedAnswer
+	}
+
+	function revealAllAnswers () {
+		questionData.map((question, index) => {
+			question.selected = true;
+			question.correct = false;
+			
+			questionData[index] = question
+		})
+	}
+
+	var selectedQuestion = questions[0];
+	console.log(selectedQuestion);
+
+	setupQuestion(selectedQuestion.answers);
+
+
 </script>
 
 <svelte:head>
@@ -7,11 +59,15 @@
 </svelte:head>
 
 <section>
-	<h2>Question Title</h2>
-	<span class="answer deselected">Answer 1</span>
-	<span class="answer deselected">Answer 2</span>
-	<span class="answer deselected">Answer 3</span>
-	<span class="answer deselected">Answer 4</span>
+	<h2>{ selectedQuestion.type == 'spelling' ? "Splat the spelling..." : "" }</h2>
+	{ #each questionData as answer }
+		<button
+			on:click|preventDefault={handleAnswerClick}
+			data-index="{ answer.index }"
+			class="answer{ answer.selected === false ? " deselected" : "" }{ answer.correct === false ? " failed" : "" }{ answer.correct === true ? " success" : "" }">
+			{ answer.answer }
+		</button>
+	{ /each }
 </section>
 
 <style>
@@ -28,6 +84,7 @@
 		flex: 1 1 100%;
 		text-align: center;
 		font-size: 2.5rem;
+		text-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.2);
 	}
 	
 	.answer {
@@ -37,10 +94,12 @@
 		background-color: var(--color-bg-0);
 		border-radius: 1rem;
 		margin: 1rem;
+		color: #FFF;
 		text-align: center;
 		font-size: 2rem;
 		transition: all 0.2s ease-in-out;
 		box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.2);
+		text-transform: capitalize;
 	}
 
 	.deselected:hover {
