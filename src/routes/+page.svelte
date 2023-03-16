2298d9b1a4016b20a032c1573eb264b03a7f29c7
David Ford <d.ford@digital-velocity.co.uk>
2023-03-16 22:57:40 +0000

    Homepage design and layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,16 @@
 <script></script>
 
 <svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+	<title>Quick Quiz</title>
+	<meta name="description" content="Quick classroom quizzes for Primary Schools" />
 </svelte:head>
 
 <section>
-	<h1>Welcome</h1>
+	<div>
+		<h1>Quick Quiz</h1>
+		<p>Quick classroom quizzes for Primary Schools.</p>
+		<p>View <a href="/quiz">all quizzes</a> or <a href="/create-quiz">click to create</a> your own.</p>
+	</div>
 </section>
 
 <style>
@@ -17,5 +21,21 @@
 		align-items: center;
 		align-content: center;
 		flex: 1;
+		background-image: url("/src/lib/images/home/welcome-blackboard.svg");
+		background-position: center;
+		background-repeat: no-repeat;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	section div {
+		margin-top: -250px;
+		max-width: 220px;
+		text-align: center;
+	}
+
+	section div h1 {
+		margin: 0;
 	}
 </style>
