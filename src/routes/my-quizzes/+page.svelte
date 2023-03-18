8ddef1b14feff346c3991930b3569b7b06b29b25
David Ford <d.ford@digital-velocity.co.uk>
2023-03-18 22:55:43 +0000

    My quizzes page setup

---
+++ src/routes/my-quizzes/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	  import { fetchUrlForKey } from "$lib/constants";
+</script>
+
+<svelte:head>
+	<title>My quizzes</title>
+</svelte:head>
+
+<section>
+	<div>
+		<h1>My Quizzes</h1>
+		<p>A list of all the quizzes saved to your machine.</p>
+		<p>No quizzes? <a href={ fetchUrlForKey('create') } title="Create a quiz">Click here</a> to create your first.</p>
+	</div>
+</section>
+
+<style lang="scss">
+</style>
