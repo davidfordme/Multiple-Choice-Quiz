44f68b0d26da7905f8ba1fbfc4f8bcf2a3ae38b2
David Ford <d.ford@digital-velocity.co.uk>
2023-03-20 12:46:57 +0000

    Lib to read from and write to local storage

---
+++ src/lib/storable.ts
@@ -0,0 +1,31 @@
+import { browser } from '$app/environment'
+import type { QuizFile } from '$lib/types/types'
+
+export function fetchFromStorage(key : string) : any | null {
+    return (browser) ? localStorage.getItem(key) : null
+}
+
+export function writeToStorage(key : string, content: any) {
+    if(browser) localStorage[key] = JSON.stringify(content)
+}
+
+export function fetchAllQuizzesFromStorage() : QuizFile[] {
+    let quizzes : QuizFile[] = []
+    if(browser) {
+        Object.keys(localStorage).map(key => {
+            if(key.includes('quiz-')) {
+                const quizString = localStorage.getItem(key)
+                if(quizString) quizzes.push(JSON.parse(quizString) as QuizFile)
+            }
+        })
+    }
+    return quizzes
+}
+
+export function fetchQuizFromStorage(key : string | null) : QuizFile | null {
+    if(key && browser) {
+        const quizString = fetchFromStorage(key)
+        if (quizString) return JSON.parse(quizString) as QuizFile
+        else return null
+    } else return null
+}
