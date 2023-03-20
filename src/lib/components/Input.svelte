b80728e7b9e2f8551918d1211f36526bfb495640
David Ford <d.ford@digital-velocity.co.uk>
2023-03-20 13:00:53 +0000

    Input element component

---
+++ src/lib/components/Input.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	export let value : string = "";
+	export let name : string;
+	export let placeholder : string;
+	export let label : string;
+</script>
+
+<label for="{ name }">
+	<p>{ label }</p>
+	<input
+		placeholder="{ placeholder }"
+		name="{ name }"
+		type="text"
+		bind:value />
+</label>
+
+<style lang="scss">
+	input {
+		border-radius: 1rem;
+		padding: 1rem;
+		background-color: transparent;
+		border: 0.2rem solid rgba(255, 255, 255, 0.45);
+		width: 100%;
+		color: #FFFFFF;
+
+		&:focus {
+			outline-color: #249FAF;
+		}
+
+		&::placeholder {
+			color: rgba(255, 255, 255, 0.45);
+		}
+	}
+</style>
